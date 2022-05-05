26adc981533a7b2cba022e1dc9d345294be7460d
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-05-05 16:10:29 +0200

    Add dbo:Language and custom level descriptions. Part of #41.

--- jobadbuilder/js/main.js
+++ jobadbuilder/js/main.js
@@ -5,18 +5,54 @@ const COMPETENCE_AREAS = {
 	ast: {
 		label: "Application system type",
 		query: `SELECT ?x (STR(?label) AS ?l) {?x hito:astClaFrom hito:MbApplicationSystemTypeCatalogue; rdfs:label ?label. FILTER(LANGMATCHES(LANG(?label),"en"))}`,
+		levels: [
+			["has basic knowledge of"],
+			["has advanced knowledge of", "has expert knowledge of"],
+			[
+				"basic skills in",
+				"advanced skills in",
+				"expert skills in",
+				"experienced with ... at basic level",
+				"experienced with ... at advanced level",
+				"experienced with ... at master level",
+				"practical experience with",
+				"hands-on experience with",
+				"safe handling of",
+				"experienced handling of",
+				"administrates",
+			],
+			["analyzes", "tests", "maintains", "administrates", "provide first level support for", "provide second level support for", "provide third level support for", "user support for"],
+			["evaluates", "assesses", "manages quality of"],
+			["plans use of", "creates", "creates a vision about the use of", "decides about ... use", "develops", "provides trainings for"],
+		],
 		endpoint: ENDPOINT_HITO,
 		property: "hito:competencyAstCla",
 	},
 	swp: {
 		label: "Software Product",
 		query: `SELECT ?x (STR(?label) AS ?l) {?x a hito:SoftwareProduct; rdfs:label ?label. FILTER(LANGMATCHES(LANG(?label),"en"))}`,
+		levels: [
+			["has basic knowledge of"],
+			["has advanced knowledge of", "has expert knowledge of"],
+			["basic skills in", "advanced skills in", "expert skills in", "experienced with ... at basic level", "experienced with ... at advanced level", "experienced with ... at master level"],
+			["analyzes", "selects", "tests", "maintains"],
+			["evaluates the use of", "assesses the use of"],
+			["plans the use of", "creates", "creates a vision about the use of", "decides about the use of"],
+		],
 		endpoint: ENDPOINT_HITO,
 		property: "hito:competencySoftwareProduct",
 	},
 	interoperability: {
 		label: "Interoperability",
 		query: `SELECT ?x (STR(?label) AS ?l) {?x a hito:Interoperability; rdfs:label ?label. FILTER(LANGMATCHES(LANG(?label),"en"))}`,
+		levels: [
+			["is interested in", "has basic knowledge of"],
+			["has advanced knowledge of", "has expert knowledge of"],
+			["basic skills in", "advanced skills in", "expert skills in"],
+			["analyzes applicability of ... in a given scenario", "selects", "tests"],
+			["evaluates the use of", "assesses the use of"],
+			["plans the use of", "creates", "creates a vision about the use of"],
+		],
 		endpoint: ENDPOINT_HITO,
 		property: "hito:competencyInteroperability",
 	},
@@ -24,18 +60,61 @@ const COMPETENCE_AREAS = {
 	programminglanguage: {
 		label: "Programming Language",
 		query: `SELECT ?x (STR(?label) AS ?l) {?x a <http://dbpedia.org/ontology/ProgrammingLanguage>; rdfs:label ?label. FILTER(LANGMATCHES(LANG(?label),"en"))}`,
+		levels: [
+			["has basic knowledge of"],
+			["has advanced knowledge of", "has expert knowledge of"],
+			[
+				"basic skills in",
+				"advanced skills in",
+				"expert skills in",
+				"experienced with ... at basic level",
+				"experienced with ... at advanced level",
+				"experienced with ... at master level",
+				"practical experience with",
+				"hands-on experience with",
+				"safe handling of",
+				"experienced handling of",
+				"administrates",
+			],
+			[/*"analyzes applicability of programming languages in a given scenario",*/ "selects", "tests ... code"],
+			["evaluates the use of", "assesses the use of"],
+			["plans the use of", "creates", "creates a vision about the use of", "decides about ... use", "develops", "provides trainings for"],
+		],
 		property: "hito:competencyProgrammingLanguage",
+		endpoint: ENDPOINT_HITO,
+	},
+	language: {
+		label: "Language",
+		query: `SELECT ?x (REPLACE(STR(?label)," language","") AS ?l) {?x a <http://dbpedia.org/ontology/Language>; rdfs:label ?label. FILTER(LANGMATCHES(LANG(?label),"en"))}`,
+		levels: [["A1 Basic User: Breakthrough"], ["A2 Basic User: Waystage"], ["B1 Independent User: Threshold"], ["B2 Independent User: Vantage"], ["C1 Proficient User: Advanced"], ["C2 Proficient User: Mastery"]],
+		property: "hito:competencyLanguage",
 		endpoint: ENDPOINT_HITO,
 	},
 	db: {
 		label: "Database System",
 		query: `SELECT ?x (STR(?label) AS ?l) {?x a hito:DatabaseSystem; rdfs:label ?label. FILTER(LANGMATCHES(LANG(?label),"en"))}`,
+		levels: [
+			["has basic knowledge of"],
+			["has advanced knowledge of", "has expert knowledge of"],
+			["applies", "basic skills in", "advanced skills in", "expert skills in", "experienced with ... at basic level", "experienced with ... at advanced level", "experienced with ... at master level"],
+			["analyzes", "tests", "administrates", "provide first level support for", "provide second level support for", "provide third level support for"],
+			["evaluates the use of", "assesses the use of", "manage the quality of"],
+			["plans the use of", "creates", "creates a vision about the use of", "decides about the use of"],
+		],
 		property: "hito:competencyDb",
 		endpoint: ENDPOINT_HITO,
 	},
 	enterpriseFunction: {
 		label: "Enterprise Function",
 		query: `SELECT ?x (STR(?label) AS ?l) {?x a hito:EnterpriseFunctionClassified; rdfs:label ?label. FILTER(LANGMATCHES(LANG(?label),"en"))}`,
+		levels: [
+			["is interested in", "has basic knowledge of"],
+			["has advanced knowledge of", "has expert knowledge of"],
+			["under construction"],
+			["analyzes processes related to", "models processes related to", "provide first level support for", "provide second level support for", "provide third level support for"],
+			["evaluates processes related to", "assesses processes related to", "evaluates the use of application systems for", "assesses the use of application systems for", "manages quality of"],
+			["creates processes related to", "designs processes related to", "provides trainings for"],
+		],
 		property: "hito:competencyEfCla",
 		endpoint: ENDPOINT_HITO,
 	},
@@ -43,6 +122,7 @@ const COMPETENCE_AREAS = {
 		label: "Role",
 		//query: `SELECT ?x (STR(?label) AS ?l) FROM <http://www.snik.eu/ontology/bb> {?x a meta:Role; rdfs:label ?label. FILTER(LANGMATCHES(LANG(?label),"en"))}`,
 		query: `SELECT ?x (STR(?label) AS ?l) FROM <http://www.snik.eu/ontology/bb> {?x rdfs:subClassOf* bb:HospitalStaff; rdfs:label ?label. FILTER(LANGMATCHES(LANG(?label),"en"))}`,
+		levels: [["fills the role of"], [], [], [], [], []],
 		property: "hito:competencyRole",
 		endpoint: ENDPOINT_SNIK,
 	},
@@ -105,12 +185,20 @@ function emptyOption(text) {
 	return emptyOption;
 }
 
-function levelOption(i) {
-	const levels = ["knows", "understands", "applies", "analyses", "evaluates", "creates"];
-	const option = document.createElement("option");
-	option.value = levels[i - 1];
-	option.innerText = i + ") " + levels[i - 1];
-	return option;
+function levelSelect(area) {
+	const levelInput = document.createElement("select");
+	levelInput.classList.add("level-select");
+	levelInput.append(emptyOption("Select competence level"));
+	for (let i = 1; i < 7; i++) {
+		for (let l of area.levels[i - 1]) {
+			const option = document.createElement("option");
+			option.value = l;
+			if (!option.value.includes("...")) option.value += " ...";
+			option.innerText = i + ") " + l;
+			levelInput.append(option);
+		}
+	}
+	return levelInput;
 }
 
 async function row(area) {
@@ -129,18 +217,15 @@ async function row(area) {
 		option.innerText = b.l.value;
 		input.append(option);
 	}
-	const levelInput = document.createElement("select");
-	levelInput.append(emptyOption("Select competence level"));
-	for (let i = 1; i < 7; i++) {
-		levelInput.append(levelOption(i));
-	}
+	const levelInput = levelSelect(area);
+
 	const addButton = document.createElement("button");
 	{
 		addButton.innerText = "Add";
 		addButton.type = "button";
 		addButton.addEventListener("click", () => {
 			if (input.value == 0 || levelInput.value == 0) return;
-			jobad.value += " * " + levelInput.value + " " + area.label + ": " + input.value + "\n";
+			jobad.value += " * " + levelInput.value.replace("...", input.value) + "\n";
 			input.value = 0;
 			levelInput.value = 0;
 		});
